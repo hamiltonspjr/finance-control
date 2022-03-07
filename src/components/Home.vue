5a257c266137b4cc035e93c1882eb4fd35c24ed6
Hamilton de Souza <hamilton-souza@outlook.com.br>
2022-03-07 10:50:16 -0300

    created form component to enter the data

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,12 +1,17 @@
 <template>
   <main>
     <h1>Controle Financeiro</h1>
+    <FormEntry />
   </main>
 </template>
 
 <script>
+import FormEntry from "./FormEntry.vue";
 export default {
   name: "Home",
+  components: {
+    FormEntry,
+  },
 };
 </script>
 
