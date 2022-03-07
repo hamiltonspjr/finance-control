21e6c7e3c2d20e2929198475d6ff1d87e2ac7196
Hamilton de Souza <hamilton-souza@outlook.com.br>
2022-03-07 19:27:13 -0300

    created method to calculate value of income and expense, created computed of income, expense and total

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -3,6 +3,9 @@
     <h1>Controle Financeiro</h1>
     <FormEntry @sendEntry="getEntry" />
     <p>{{ allEntry }}</p>
+    <p>{{ incomeValue }}</p>
+    <p>{{ expenseValue }}</p>
+    <p>{{ total }}</p>
   </main>
 </template>
 
@@ -15,15 +18,37 @@ export default {
   },
   data() {
     return {
-      incomeValue: null,
-      expenseValue: null,
       allEntry: [],
-      total: null,
     };
   },
   methods: {
     getEntry(event) {
       this.allEntry.push(event);
+    },
+    calculateValue(array, category) {
+      let value = 0;
+      const data = array
+        .filter((item) => item.category === category)
+        .map((item) => {
+          return item.value;
+        });
+      data.forEach((item) => {
+        value += item;
+      });
+      return value;
+    },
+  },
+  computed: {
+    incomeValue() {
+      let value = this.calculateValue(this.allEntry, "income");
+      return value;
+    },
+    expenseValue() {
+      let value = this.calculateValue(this.allEntry, "expense");
+      return value;
+    },
+    total() {
+      return this.incomeValue - this.expenseValue;
     },
   },
 };
