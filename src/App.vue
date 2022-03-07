1d68927112f0f3f415d470e19ffd4892e065bf4a
Hamilton de Souza <hamilton-souza@outlook.com.br>
2022-03-07 10:12:27 -0300

    initial settings and created home component

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,32 @@
-<script setup></script>
+<script>
+import Home from "./components/Home.vue";
+export default {
+  components: {
+    Home,
+  },
+};
+</script>
 
 <template>
-  <p>Finance Control</p>
+  <Home />
 </template>
 
 <style>
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
+}
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+html {
+  font-size: 62.5%;
+}
+a {
+  text-decoration: none;
+}
+ul {
+  list-style: none;
 }
 </style>
---
+++ src/components/Home.vue
@@ -0,0 +1,13 @@
+<template>
+  <main>
+    <h1>Controle Financeiro</h1>
+  </main>
+</template>
+
+<script>
+export default {
+  name: "Home",
+};
+</script>
+
+<style></style>
