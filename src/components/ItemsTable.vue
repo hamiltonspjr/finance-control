29a88159cfcb587a64645a42ada78f57a0749d5a
Hamilton de Souza <hamilton-souza@outlook.com.br>
2022-03-08 15:10:25 -0300

    created table component to show them the entries, created funcion to modify the date in helpers.js

---
+++ src/components/ItemsTable.vue
@@ -0,0 +1,51 @@
+<template>
+  <section>
+    <div v-if="newList && newList.length">
+      <table>
+        <thead>
+          <tr>
+            <th>Data</th>
+            <th>Título</th>
+            <th>Categoria</th>
+            <th>Valor</th>
+            <th></th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr v-for="(item, index) in newList" :key="item.title + index">
+            <td>{{ item.date }}</td>
+            <td>{{ item.title }}</td>
+            <td>{{ item.category }}</td>
+            <td>{{ item.value }}</td>
+            <td>excluir</td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+    <div v-else>
+      <p>Nenhuma entrada inserida no momento.</p>
+    </div>
+  </section>
+</template>
+
+<script>
+import { valueFormatBrl, dateFormatBr } from "../helpers.js";
+export default {
+  name: "ItemsTable",
+  props: ["list"],
+  computed: {
+    newList() {
+      return this.list.map((item) => {
+        return {
+          date: dateFormatBr(item.date),
+          title: item.title,
+          category: item.category,
+          value: valueFormatBrl(item.value),
+        };
+      });
+    },
+  },
+};
+</script>
+
+<style></style>
